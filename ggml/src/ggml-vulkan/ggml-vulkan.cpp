961c6caecd707808aa381f52df38abf65d4b6c9e
gianni-cor <gianfrancocordella@gmail.com>
2025-09-12 08:50:57 +0200

    Amend vk profiling triplet

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -1927,6 +1927,7 @@ class vk_perf_logger {
         if (timings.size() > 0) {
             ss << "Total time: " << total_all_op_times / 1000.0 << " us." << std::endl;
         }
+        ss << std::endl;
         GGML_LOG_DEBUG("%s", ss.str().c_str());
     }
 
@@ -1977,8 +1978,8 @@ class vk_perf_logger {
             ss << "Total operation types: " << operation_groups.size() << std::endl;
             ss << "Total variations: " << triplet_timings.size() << std::endl;
             ss << std::endl;
-        } catch (...) {
             GGML_LOG_DEBUG("%s", ss.str().c_str());
+        } catch (...) {
             GGML_LOG_DEBUG("Error in triplet timing analysis - analysis skipped.\n");
         }
     }
