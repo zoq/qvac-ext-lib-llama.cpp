7403872f8e977b4c3cdf564f5efe9959304d994a
Jesús <jesus.mb.1995@gmail.com>
2025-09-30 14:50:53 +0200

    remove_check

--- ggml/include/gguf.h
+++ ggml/include/gguf.h
@@ -200,7 +200,7 @@ extern "C" {
 }
 #endif
 
-#if defined(__cplusplus) && __cplusplus >= 201703L
+#if defined(__cplusplus)
 #include <streambuf>
 GGML_API struct gguf_context * gguf_init_from_buffer(std::basic_streambuf<char>& streambuf, struct gguf_init_params params);
 #endif
