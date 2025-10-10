be13dc03ccffeeca2f5b6e3ecc7d19c5ab339cd2
Italo Nicola <italo.nicola@collabora.com>
2025-10-10 19:29:17 -0300

    Vulkan: disable rms_norm fusion on Adreno
    This optimization broke inference on Adreno.

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -5594,7 +5594,8 @@ static vk_device ggml_vk_get_device(size_t idx) {
 
         device->idx = idx;
 
-        device->disable_fusion = getenv("GGML_VK_DISABLE_FUSION") != nullptr;
+        device->disable_fusion = getenv("GGML_VK_DISABLE_FUSION") != nullptr ||
+                                 device->vendor_id == VK_VENDOR_ID_QUALCOMM;
 
         device->add_rms_fusion = !device->disable_fusion &&
                                  device->subgroup_arithmetic &&
