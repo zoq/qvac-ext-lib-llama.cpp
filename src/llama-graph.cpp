3ecac5d2cfbfa9187f689fabed47f0018f193702
makaveli10 <vineet.suryan@collabora.com>
2026-03-06 19:28:02 +0530

    graph: restore ggml_view_4d for non-contiguous Q tensor support
    Signed-off-by: makaveli10 <vineet.suryan@collabora.com>

--- src/llama-graph.cpp
+++ src/llama-graph.cpp
@@ -1744,7 +1744,7 @@ ggml_tensor * llm_graph_context::build_attn_mha(
     // split the batch into streams if needed
     const auto n_stream = k->ne[3];
 
-    q = ggml_reshape_4d(ctx0, q, q->ne[0], q->ne[1], q->ne[2]/n_stream, n_stream);
+    q = ggml_view_4d(ctx0, q, q->ne[0], q->ne[1], q->ne[2]/n_stream, n_stream, q->nb[1], q->nb[2], q->nb[3]/n_stream, 0);
 
     q = ggml_permute(ctx0, q, 0, 2, 1, 3);
     k = ggml_permute(ctx0, k, 0, 2, 1, 3);
